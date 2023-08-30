de8a1d5eea14266e96eebc36168a46def5671faa
Tony Francis <tony@dream3d.com>
2023-08-30 19:15:49 -0400

    add scheduler plugin

--- dask_plugins/ray.py
+++ dask_plugins/ray.py
@@ -1,7 +1,25 @@
-from distributed import WorkerPlugin
+from distributed import WorkerPlugin, SchedulerPlugin
 
 
 class RayWorkerPlugin(WorkerPlugin):
+    name = "ray"
+
+    def __init__(self, ray_address: str, namespace: str | None = None):
+        self.ray_address = ray_address
+        self.namespace = namespace
+
+    def setup(self, worker):
+        import ray
+
+        ray.init(address=self.ray_address, namespace=self.namespace)
+
+    def teardown(self, worker):
+        import ray
+
+        ray.shutdown()
+
+
+class RaySchedulerPlugin(SchedulerPlugin):
     name = "ray"
 
     def __init__(self, ray_address: str, namespace: str | None = None):
