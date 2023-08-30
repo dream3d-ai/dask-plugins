60f15af02d43dc0a41b184fbc1fc65186f698625
Tony Francis <tony@dream3d.com>
2023-08-30 17:08:10 -0400

    ray plugin

---
+++ dask_plugins/ray.py
@@ -0,0 +1,19 @@
+from distributed import WorkerPlugin
+
+
+class RayWorkerPlugin(WorkerPlugin):
+    name = "ray"
+
+    def __init__(self, ray_address: str, namespace: str | None = None):
+        self.ray_address = ray_address
+        self.namespace = namespace
+
+    def setup(self, worker):
+        import ray
+
+        ray.init(address=self.ray_address, namespace=self.namespace)
+
+    def teardown(self, worker):
+        import ray
+
+        ray.shutdown()
