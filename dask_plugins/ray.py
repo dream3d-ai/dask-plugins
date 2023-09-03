551f286c1a117cfbadccc9eda62ebf861ad8b9d4
Tony Francis <tony@dream3d.com>
2023-09-02 20:41:13 -0400

    add runtime env

--- dask_plugins/ray.py
+++ dask_plugins/ray.py
@@ -1,4 +1,4 @@
-from distributed import WorkerPlugin, SchedulerPlugin
+from distributed import SchedulerPlugin, WorkerPlugin
 
 
 class RayWorkerPlugin(WorkerPlugin):
@@ -22,16 +22,42 @@ class RayWorkerPlugin(WorkerPlugin):
 class RaySchedulerPlugin(SchedulerPlugin):
     name = "ray"
 
-    def __init__(self, ray_address: str, namespace: str | None = None):
+    def __init__(
+        self,
+        ray_address: str,
+        packages: str | None = None,
+        namespace: str | None = None,
+    ):
         self.ray_address = ray_address
         self.namespace = namespace
 
-    def setup(self, worker):
+        self.packages = NOne
+        if packages is not None:
+            with open(packages, "r") as f:
+                self.packages = f.read()
+
+    def setup(self, scheduler):
         import ray
 
-        ray.init(address=self.ray_address, namespace=self.namespace)
+        runtime_env = None
+        if self.packages is not None:
+            import tempfile
+            import zipfile
 
-    def teardown(self, worker):
+            with tempfile.NamedTemporaryFile() as f:
+                f.write(self.packages)
+                f.flush()
+                with zipfile.ZipFile(f.name, "r") as zip_ref:
+                    zip_ref.extractall("/tmp/user-packages")
+            runtime_env = {"working_dir": "/tmp/user-packages"}
+
+        ray.init(
+            address=self.ray_address,
+            namespace=self.namespace,
+            runtime_env=runtime_env,
+        )
+
+    def teardown(self, scheduler):
         import ray
 
         ray.shutdown()
