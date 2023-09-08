30e00316eac9aa03bd33497611192777c9f550b6
Tony Francis <tony@dream3d.com>
2023-09-08 10:16:55 -0400

    kwargs

--- dask_plugins/ray.py
+++ dask_plugins/ray.py
@@ -4,14 +4,24 @@ from distributed import SchedulerPlugin, WorkerPlugin
 class RayWorkerPlugin(WorkerPlugin):
     name = "ray"
 
-    def __init__(self, ray_address: str, namespace: str | None = None):
+    def __init__(
+        self,
+        ray_address: str,
+        namespace: str | None = None,
+        **kwargs,
+    ):
         self.ray_address = ray_address
         self.namespace = namespace
+        self.kwargs = kwargs
 
     def setup(self, worker):
         import ray
 
-        ray.init(address=self.ray_address, namespace=self.namespace)
+        ray.init(
+            address=self.ray_address,
+            namespace=self.namespace,
+            **kwargs,
+        )
 
     def teardown(self, worker):
         import ray
@@ -27,34 +37,20 @@ class RaySchedulerPlugin(SchedulerPlugin):
         ray_address: str,
         packages: str | None = None,
         namespace: str | None = None,
+        **kwargs,
     ):
         self.ray_address = ray_address
         self.namespace = namespace
-
-        self.packages = NOne
-        if packages is not None:
-            with open(packages, "r") as f:
-                self.packages = f.read()
+        self.object_store_memory = object_store_memory
+        self.kwargs = kwargs
 
     def setup(self, scheduler):
         import ray
 
-        runtime_env = None
-        if self.packages is not None:
-            import tempfile
-            import zipfile
-
-            with tempfile.NamedTemporaryFile() as f:
-                f.write(self.packages)
-                f.flush()
-                with zipfile.ZipFile(f.name, "r") as zip_ref:
-                    zip_ref.extractall("/tmp/user-packages")
-            runtime_env = {"working_dir": "/tmp/user-packages"}
-
         ray.init(
             address=self.ray_address,
             namespace=self.namespace,
-            runtime_env=runtime_env,
+            **kwargs,
         )
 
     def teardown(self, scheduler):
